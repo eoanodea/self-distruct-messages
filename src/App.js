582ed1ee55e7d83afb6a95c6d582accdf021a7cc
Eoan O Dea <odeaeoan@gmail.com>
2020-11-28 15:30:07 +0100

    Created simple static frontend for pass code app

--- src/App.js
+++ src/App.js
@@ -1,22 +1,57 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.gif";
+import unlocked from "./unlocked.gif";
+import "./App.css";
 
 function App() {
+  const [correct, setCorrect] = React.useState(false);
+  const [input, setInput] = React.useState("");
+  const [error, setError] = React.useState(null);
+  const [passcode, setPasscode] = React.useState("hello");
+
+  const submit = () => {
+    if (input !== passcode) return setError("Passphrase is not correct");
+
+    setError(null);
+    setCorrect(true);
+  };
+
+  const erase = () => {
+    setCorrect(false);
+    setPasscode(Math.random().toString(36).substring(7));
+  };
+
+  if (correct)
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={unlocked} className="App-logo" alt="logo" />
+
+          <p>Secure Message:</p>
+          <p className="message">This is a secret message</p>
+          <button className="erase-button" onClick={erase}>
+            Destroy
+          </button>
+        </header>
+      </div>
+    );
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>Enter your passphrase to continue</p>
+        <div className="form-container">
+          <input
+            type="text"
+            id="passcode"
+            value={input}
+            onChange={(v) => setInput(v.target.value)}
+            placeholder="Passphrase"
+          />
+
+          <button onClick={submit}>Enter</button>
+        </div>
+        {error && <p className="error"> {error}</p>}
       </header>
     </div>
   );
